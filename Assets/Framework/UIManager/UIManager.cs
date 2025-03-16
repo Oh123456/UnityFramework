bff0a7f4c39f22fde5a59298c92fdb012c6abf21
OJK <dhwnsrud98@gmail.com>
2025-03-16 18:25:23 +0900

    UIManager 제작

---
+++ Assets/Framework/UIManager/UIManager.cs
@@ -0,0 +1,111 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityFramework.Singleton;
+
+namespace UnityFramework.UI
+{
+    public class UIManager : LazySingleton<UIManager>
+    {
+        public class UIController 
+        {
+            UIBase uIBase;
+
+            public System.Action Show;
+            public System.Action Hide;
+
+            public UIBase UIBase => this.uIBase;
+
+            public void Initialize(UIBase uIBase)
+            {
+                this.uIBase = uIBase;
+                this.uIBase.AddListener(this);
+            }
+
+            public void Release()
+            {
+                uIBase = null;
+                Show = null;
+                Hide = null;                
+            }
+
+        }
+
+        private Dictionary<System.Type, UIBase> uis = new Dictionary<System.Type, UIBase>();
+        private Stack<UIController> showUIStack = new Stack<UIController>(4);
+        private Stack<UIController> controllerPool = new Stack<UIController>(4);
+
+
+        public T Show<T>(string name) where T : UIBase
+        {
+            T ui = GetCachedUI<T>(name);
+            UIController uIController = GetUIController();
+            uIController.Initialize(ui);
+            uIController.Show();
+            showUIStack.Push(uIController);
+            return ui;
+        }
+
+        public void Hide()
+        {
+
+            if (GetActiveUIController(out UIController uIController))
+            {
+                uIController.Hide();
+                uIController.Release();
+                controllerPool.Push(uIController);
+            }
+          
+        }
+
+        public void Close()
+        {
+            if (GetActiveUIController(out UIController uIController))
+            {
+                uIController.Hide();
+                uIController.UIBase.Close();
+                uIController.Release();
+                controllerPool.Push(uIController);
+            }
+        }
+
+        private bool GetActiveUIController(out UIController uIController)
+        {
+            uIController = null;
+            if (showUIStack.Count == 0)
+                return false;
+            uIController = showUIStack.Pop();
+            return true;
+        }
+
+        public bool GetCachedUI<T>(string name, out T ui) where T : UIBase
+        {
+            bool result = uis.TryGetValue(typeof(T), out var baseUI);
+            if (baseUI == null)
+                result = false;
+            ui = result ? (T)baseUI : null;
+            return result;
+        }
+
+        private T GetCachedUI<T>(string name) where T : UIBase
+        {
+            T ui = null;
+            if (!GetCachedUI(name, out ui))
+            {
+                T prb = Resources.Load<T>(name);
+                ui = GameObject.Instantiate<T>(prb);
+                uis[typeof(T)]= ui;
+            }
+
+            return ui;
+        }
+
+        private UIController GetUIController()
+        {
+            if (controllerPool.Count == 0)
+                return new UIController();
+
+            return controllerPool.Pop();
+        }
+    }
+}
